d98b52de3e4a63101f53f3d4c4cd9d111e4eebb3
Ana Moreno <ana.moreno@codeup.com>
2023-03-02 05:30:54 -0600

    completed media css

--- script.js
+++ script.js
@@ -46,39 +46,41 @@ $(document).ready(function(){
    
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Birthdays", "Anniversaries", "Graduations"],
+        strings: ["Birthdays", "Anniversary", "Graduations"],
         typeSpeed: 100,
         backSpeed: 60,
         
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing-2", {
+    //     strings: ["Developer", "Designer", "Freelancer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 
     // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+    // $('.carousel').owlCarousel({
+    //     margin: 20,
+    //     loop: true,
+    //     autoplayTimeOut: 2000,
+    //     autoplayHoverPause: true,
+    //     responsive: {
+    //         0:{
+    //             items: 1,
+    //             nav: false
+    //         },
+    //         600:{
+    //             items: 2,
+    //             nav: false
+    //         },
+    //         1000:{
+    //             items: 3,
+    //             nav: false
+    //         }
+    //     }
+    // });
+
+
 });
